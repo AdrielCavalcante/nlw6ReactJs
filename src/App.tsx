83400b2e1ad94e378c4df29b54cfcb490c4de3b8
Adriel Cavalcante <72228497+AdrielCavalcante@users.noreply.github.com>
2021-06-24 21:40:19 -0300

    Add Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
+import { Room } from './pages/Room';
 
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
@@ -11,9 +12,12 @@ function App() {
 
 	return (
 		<BrowserRouter>
-      <AuthContextProvider>
-				<Route path="/" exact component={Home} />
-				<Route path="/rooms/new" component={NewRoom} />
+			<AuthContextProvider>
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/rooms/new" component={NewRoom} />
+					<Route path="/rooms/:id" component={Room} />
+				</Switch>
 			</AuthContextProvider>
 		</BrowserRouter>
 	);
