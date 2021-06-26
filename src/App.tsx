66160bfe2c503182dbec1c8cd8f3a66b3c71c2ab
Adriel Cavalcante <72228497+AdrielCavalcante@users.noreply.github.com>
2021-06-26 15:54:10 -0300

    Add aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Room } from './pages/Room';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
+import { AdminRoom } from './pages/AdminRoom';
 
 // Adicionar dominio da Vercel no Firebase authentication
 function App() {
@@ -17,6 +18,8 @@ function App() {
 					<Route path="/" exact component={Home} />
 					<Route path="/rooms/new" component={NewRoom} />
 					<Route path="/rooms/:id" component={Room} />
+
+					<Route path="/admin/rooms/:id" component={AdminRoom} />
 				</Switch>
 			</AuthContextProvider>
 		</BrowserRouter>
